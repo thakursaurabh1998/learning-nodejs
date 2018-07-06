cf3eb9586b1b33436cace82f68b37318d0b26a53
Saurabh Thakur <thakursaurabh1998@gmail.com>
2018-07-06 18:01:52 +0530

    using npm module

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,7 +1,12 @@
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
 
 console.log('app.js');
+console.log(_.isString("Saurabh thakur"));
+console.log(_.isString([1,2,3]));
+
+console.log(_.uniq([1,2,2,1,3,45,6,4,2]));
 
 
 fs.appendFileSync('text.txt',`hello ${os.userInfo().username}`);

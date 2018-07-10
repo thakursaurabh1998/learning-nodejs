29fec62cbda412bf745d6bf97ff5f7f6406e9c6d
Saurabh Thakur <thakursaurabh1998@gmail.com>
2018-07-10 18:47:19 +0530

    Test added to /todos GET route

--- node-todo-api/server/tests/server.test.js
+++ node-todo-api/server/tests/server.test.js
@@ -4,8 +4,19 @@ const request = require("supertest");
 const { app } = require("../server");
 const { Todo } = require("../models/models");
 
+const todos = [
+  {
+    text: "First item"
+  },
+  {
+    text: "Second item"
+  }
+];
+
 beforeEach(done => {
-  Todo.remove({}).then(() => done());
+  Todo.remove({})
+    .then(() => Todo.insertMany(todos))
+    .then(() => done());
 });
 
 describe("POST /todos", () => {
@@ -22,7 +33,7 @@ describe("POST /todos", () => {
       .end((err, res) => {
         if (err) return done(err);
 
-        Todo.find()
+        Todo.find({ text })
           .then(todos => {
             expect(todos.length).toBe(1);
             expect(todos[0].text).toBe(text);
@@ -42,10 +53,23 @@ describe("POST /todos", () => {
 
         Todo.find()
           .then(todos => {
-            expect(todos.length).toBe(0);
+            expect(todos.length).toBe(2);
             done();
           })
           .catch(e => done(e));
       });
+  });
+});
+
+describe("GET /todos", () => {
+  it("should get all todos", done => {
+    request(app)
+      .get("/todos")
+      .expect(200)
+      .expect(res => {
+        expect(res.body.todos.length)
+          .toBe(2);
+      })
+      .end(done);
   });
 });

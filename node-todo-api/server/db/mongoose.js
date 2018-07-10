117877f25809d337689e626cddfd02a7f13ddaa9
Saurabh Thakur <thakursaurabh1998@gmail.com>
2018-07-10 18:04:33 +0530

    Test added to /todo POST route

--- node-todo-api/server/db/mongoose.js
+++ node-todo-api/server/db/mongoose.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 
 const url =
-  "mongodb://thakursaurabh1998:helloworld123@ds131531.mlab.com:31531/todoapp";
-  // "mongodb://127.0.0.1:27017/todoapp";
+  // "mongodb://thakursaurabh1998:helloworld123@ds131531.mlab.com:31531/todoapp";
+  "mongodb://127.0.0.1:27017/todoapp";
 
 mongoose.Promise = global.Promise;
 mongoose.connect(

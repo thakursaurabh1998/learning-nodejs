81b01375c71dfc45a63d19161ef57cdf9cb57eb5
Saurabh Thakur <thakursaurabh1998@gmail.com>
2018-07-12 21:24:47 +0530

    password hashing added

--- node-todo-api/server/models/models.js
+++ node-todo-api/server/models/models.js
@@ -2,6 +2,7 @@ const { mongoose } = require("../db/mongoose");
 const validator = require("validator");
 const jwt = require("jsonwebtoken");
 const _ = require("lodash");
+const bcrypt = require("bcryptjs");
 
 let UserSchema = new mongoose.Schema({
   email: {
@@ -69,6 +70,22 @@ UserSchema.statics.findByToken = function(token) {
     "tokens.access": "auth"
   });
 };
+
+UserSchema.pre("save", function(next) {
+  const user = this;
+
+  if (user.isModified("password")) {
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        if (err) return;
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
+});
 
 const Todo = mongoose.model("todo", {
   text: {
